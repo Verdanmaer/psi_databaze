f754cc57d398ed2c49c8dfcb4565fb217c1ed33d
Adam Verner <verneradam@gmail.com>
2021-05-18 21:02:01 +0200

    mongo update

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -5,40 +5,45 @@ const router = express.Router();
 
 // Get Posts
 router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    res.send(await posts.find({}).toArray());
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 
 // Add Posts
 router.post('/', async (req, res) => {
-    const posts = await loadPostsCollection();
-    await posts.insertOne({
-        race: req.body.race,
-        sex: req.body.sex,
-        age: req.body.age,
-        createdAt: new Date()
-    });
-    res.status(201).send();
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    disappearTime: req.body.disappearTime,
+    disappearPlace: req.body.disappearPlace,
+    race: req.body.race,
+    color: req.body.color,
+    age: req.body.age,
+    name: req.body.name,
+    info: req.body.info,
+    // imgUrl: req.body.imgUrl,
+    createdAt: new Date()
+  });
+  res.status(201).send();
 })
 
 
 // Delete Post
 router.delete('/:id', async (req, res) => {
-    const posts = await loadPostsCollection();
-    await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
-    res.status(200).send();
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send();
 });
 
 
 async function loadPostsCollection() {
-    const client = await mongodb.MongoClient.connect
-    ('mongodb+srv://adam:adam@cluster0.doni8.mongodb.net/mevn?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+  const client = await mongodb.MongoClient.connect
+  ('mongodb+srv://adam:adam@cluster0.doni8.mongodb.net/mevn?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-    return client.db('vue_express').collection('posts');
+  return client.db('vue_express').collection('posts');
 }
 
 
