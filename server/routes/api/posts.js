edf1c6086494d43c92c5f7ecb15c404d2046f4e2
Adam Verner <verneradam@gmail.com>
2021-07-18 18:43:15 +0200

    test

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Set Storage Engine
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './public');
+    cb(null, './server/public/img');
   },
   filename: function(req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname).toLowerCase());
